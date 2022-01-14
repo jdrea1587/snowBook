8b016187c49a4744c704f80d1eb7d67e5fd2703c
Soul Lee <lee.soul12@gmail.com>
2022-01-14 11:52:31 -0500

    finish components and work on styling

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Footer = () => {
-  return <div>This is Footer</div>;
+  return <div className="footer">This is Footer</div>;
 };
 
 export default Footer;
---
+++ client/src/components/Profile.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Profile = () => {
+  return (
+    <div className="profile">
+      This is profile with the form to make a profile.
+    </div>
+  );
+};
+
+export default Profile;
