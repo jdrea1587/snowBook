70c3ffb30c4202cda177f157fd4d3fbe4ab92f26
Michael Newman <therealmichaelnewman@gmail.com>
2022-01-15 10:06:42 -0500

    update Profile.js and App.js

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react";
 
 const Home = () => {
   return (
@@ -13,6 +13,21 @@ const Home = () => {
       <ul>name 8</ul>
       <ul>name 9</ul>
       <ul>name 10</ul>
+
+      <div className="home-profiles">
+        <h1>FIRST AND LAST NAME HERE</h1>
+        <h2>Gender: GENDER HERE</h2>
+        <h2>Zip code: ZIP CODE HERE</h2>
+        <h2>Type of ride: TYPE OF RIDE HERE</h2>
+        <h2>Interest: INTEREST HERE</h2>
+        <h2>Level: LEVEL HERE</h2>
+        <h2>Backcountry: YES OR NO HERE</h2>
+        <h2>Personal Story: </h2>
+        <p>PERSONAL STORY HERE</p>
+        <h3>Contact: CONTACT HERE</h3>
+        <h3>Instructor: YES OR NO HERE</h3>
+        <h3>Over 21: YES OR NO HERE</h3>
+      </div>
     </div>
   );
 };
