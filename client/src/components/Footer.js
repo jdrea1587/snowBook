37a4c79659184abeb306e8925a05d5827212f802
RicoWilliams <ricowilliams@protonmail.com>
2022-01-16 10:19:32 -0700

    added social links as icons to footer

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -2,9 +2,35 @@ import React from 'react';
 
 const Footer = () => {
   return (
-    <div className="footer">
-      <div>this is footer</div>
-      <div>contact goes in here</div>
+    <div>
+      <nav className="footer">
+        <a
+          href="https://github.com/RobWilliams0615"
+          className="fa fa-github"
+        ></a>
+        <a href="https://github.com/sal59265" className="fa fa-github"></a>
+        <a href="https://github.com/jdrea1587" className="fa fa-github"></a>
+        <a
+          href="https://github.com/therealmikenew"
+          className="fa fa-github"
+        ></a>
+        <a
+          href="https://www.linkedin.com/in/robwilliamsdev"
+          className="fa fa-linkedin"
+        ></a>
+        <a
+          href="https://www.linkedin.com/in/soul-lee/"
+          className="fa fa-linkedin"
+        ></a>
+        <a
+          href="https://www.linkedin.com/in/joshua-rea/"
+          className="fa fa-linkedin"
+        ></a>
+        <a
+          href="https://www.linkedin.com/in/therealmichaelnewman/"
+          className="fa fa-linkedin"
+        ></a>
+      </nav>
     </div>
   );
 };
