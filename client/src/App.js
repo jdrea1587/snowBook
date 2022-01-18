c543f1c103618cea7357bd87dd838bbf77b921c7
Michael Newman <therealmichaelnewman@gmail.com>
2022-01-18 15:04:40 -0500

    update Store.js, StoreDetail.js, City.js, Mountain.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,38 +1,43 @@
-import { useEffect, useState } from 'react';
-import './App.css';
-import Nav from './components/Nav';
-import Home from './components/Home';
-import Footer from './components/Footer';
-import { Route } from 'react-router-dom';
-import City from './components/City';
-import Profile from './components/Profile';
-import ProfileDetail from './components/ProfileDetail';
-import Store from './components/Store';
-import Mountain from './components/Mountain';
-import StoreDetail from './components/StoreDetail';
-import axios from 'axios';
+import { useEffect, useState } from "react";
+import "./App.css";
+import Nav from "./components/Nav";
+import Home from "./components/Home";
+import Footer from "./components/Footer";
+import { Route } from "react-router-dom";
+import City from "./components/City";
+import Profile from "./components/Profile";
+import ProfileDetail from "./components/ProfileDetail";
+import Store from "./components/Store";
+import Mountain from "./components/Mountain";
+import StoreDetail from "./components/StoreDetail";
+import axios from "axios";
 
 function App() {
-  const [mountains, setMountains] = useState([]);
+  const [cities, setCities] = useState([]);
   const [storeDetails, setStoreDetails] = useState([]);
   const [profile, setProfile] = useState([]);
   const [store, setStore] = useState([]);
   const [home, setHome] = useState([]);
 
   const getHome = async () => {
-    const response = await axios.get('http://localhost:3001/api/users');
-    console.log(response.data.users);
+    const response = await axios.get("http://localhost:3001/api/users");
     setHome(response.data.users);
   };
 
   const getStore = async () => {
-    const response = await axios.get('http://localhost:3001/api/skistores');
+    const response = await axios.get("http://localhost:3001/api/skistores");
     setStore(response.data.skistores);
+  };
+
+  const getCities = async () => {
+    const response = await axios.get("http://localhost:3001/api/cities");
+    setCities(response.data.cities);
   };
 
   useEffect(() => {
     getHome();
     getStore();
+    getCities();
   }, []);
 
   return (
@@ -63,7 +68,11 @@ function App() {
           path="/profiledetail/:id"
           component={(props) => <ProfileDetail {...props} />}
         />
-        <Route exact path="/city" component={City} />
+        <Route
+          exact
+          path="/city"
+          component={(props) => <City {...props} cities={cities} />}
+        />
         <Route
           exact
           path="/store"
@@ -73,7 +82,7 @@ function App() {
         <Route
           exact
           path="/mountain/:id"
-          component={(props) => <Mountain {...props} mountains={mountains} />}
+          component={(props) => <Mountain {...props} />}
         />
         <Route
           exact
